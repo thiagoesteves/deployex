5fca68e2103616e5d6a26a0d23d9edfcacb02769
Thiago Esteves <thiago.esteves@erlang-solutions.com>
2024-05-10 17:28:04 -0300

    b

--- mix.exs
+++ mix.exs
@@ -71,7 +71,8 @@ defmodule Deployex.MixProject do
       {:hackney, "~> 1.9"},
       {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false}
     ]
   end
 end
