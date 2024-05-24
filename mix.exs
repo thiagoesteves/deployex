f703bb2292443491e8ab5da61cbaa38eb13e2b00
Thiago Esteves <calori@gmail.com>
2024-05-24 11:12:51 -0300

    Changing env.rel.sh to allow changing the cookie by ENV VAR (#3)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Deployex.MixProject do
   def project do
     [
       app: :deployex,
-      version: "0.2.0",
+      version: "0.2.1-rc1",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
