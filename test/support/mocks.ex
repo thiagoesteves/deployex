1b56fbd04acca898197518bc8c6088bd603c820f
Thiago Esteves <calori@gmail.com>
2024-08-10 10:18:50 -0300

    Add mock for Status

--- test/support/mocks.ex
+++ test/support/mocks.ex
@@ -1,2 +1,3 @@
 Mox.defmock(Deployex.MonitorMock, for: Deployex.Monitor.Adapter)
+Mox.defmock(Deployex.StatusMock, for: Deployex.Status.Adapter)
 Mox.defmock(Deployex.ReleaseMock, for: Deployex.Release.Adapter)
