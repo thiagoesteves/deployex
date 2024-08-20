cc9646130ed26393a96440c0a41f77cf43b7e470
Thiago Esteves <calori@gmail.com>
2024-08-20 12:14:40 -0300

    mix format

--- mix.exs
+++ mix.exs
@@ -124,7 +124,7 @@ defmodule Deployex.MixProject do
       {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:mox, "~> 1.0", only: :test},
-      {:excoveralls, "~> 0.18", only: :test},
+      {:excoveralls, "~> 0.18", only: :test}
     ]
   end
 
