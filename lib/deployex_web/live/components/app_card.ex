cd2c4e93c28b50ae0fe38fad850d6fb2ef3d01e8
Thiago Esteves <calori@gmail.com>
2024-09-01 17:49:28 -0300

    Adjusted terminal buttom

--- lib/deployex_web/live/components/app_card.ex
+++ lib/deployex_web/live/components/app_card.ex
@@ -112,7 +112,7 @@ defmodule DeployexWeb.Components.AppCard do
             phx-value-instance={@instance}
             phx-value-std="terminal"
             type="button"
-            class="ml-2  me-2 mb-2 text-white bg-gradient-to-r from-gray-500 to-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            class="ml-2 me-2 mb-2 text-white bg-gradient-to-r from-gray-500 to-gray-800 font-medium rounded-lg text-sm px-3 py-1.5 text-center"
           >
             <svg
               width="16px"
