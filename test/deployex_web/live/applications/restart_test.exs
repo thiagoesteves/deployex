9ce97f2052eaefdaa2f289d10d8a115aecddc9a5
Thiago Esteves <calori@gmail.com>
2024-10-10 21:27:15 -0300

    Adding reset button for deployex (#74)

--- test/deployex_web/live/applications/restart_test.exs
+++ test/deployex_web/live/applications/restart_test.exs
@@ -2,6 +2,7 @@ defmodule DeployexWeb.Applications.RestartTest do
   use DeployexWeb.ConnCase, async: false
 
   import Phoenix.LiveViewTest
+  import ExUnit.CaptureLog
   import Mox
 
   setup [
@@ -13,7 +14,7 @@ defmodule DeployexWeb.Applications.RestartTest do
   alias Deployex.Fixture.Monitoring
   alias Deployex.Fixture.Status, as: FixtureStatus
 
-  test "Click restart Button, but cancel the operation", %{conn: conn} do
+  test "Click restart Button, but cancel the operation - instance 1", %{conn: conn} do
     Deployex.StatusMock
     |> expect(:monitoring, fn -> {:ok, Monitoring.list()} end)
     |> expect(:subscribe, fn -> :ok end)
@@ -29,7 +30,7 @@ defmodule DeployexWeb.Applications.RestartTest do
     refute has_element?(index_live, "#cancel-button-1")
   end
 
-  test "Click restart Button, confirm the operation", %{conn: conn} do
+  test "Click restart Button, confirm the operation - instance 1", %{conn: conn} do
     Deployex.StatusMock
     |> expect(:monitoring, fn -> {:ok, Monitoring.list()} end)
     |> expect(:subscribe, fn -> :ok end)
@@ -46,5 +47,42 @@ defmodule DeployexWeb.Applications.RestartTest do
     assert index_live |> element("#confirm-button-1", "Confirm") |> render_click()
 
     refute has_element?(index_live, "#cancel-button-1")
+  end
+
+  test "Click restart Button, but cancel the operation - deployex", %{conn: conn} do
+    Deployex.StatusMock
+    |> expect(:monitoring, fn -> {:ok, Monitoring.list()} end)
+    |> expect(:subscribe, fn -> :ok end)
+    |> stub(:history_version_list, fn -> FixtureStatus.versions() end)
+
+    {:ok, index_live, _html} = live(conn, ~p"/applications")
+
+    assert index_live |> element("#app-restart-0") |> render_click() =~
+             "Are you sure you want to restart deployex?"
+
+    assert index_live |> element("#cancel-button-0", "Cancel") |> render_click()
+
+    refute has_element?(index_live, "#cancel-button-0")
+  end
+
+  test "Click restart Button, confirm the operation - deployex", %{conn: conn} do
+    Deployex.StatusMock
+    |> expect(:monitoring, fn -> {:ok, Monitoring.list()} end)
+    |> expect(:subscribe, fn -> :ok end)
+    |> stub(:history_version_list, fn -> FixtureStatus.versions() end)
+
+    Deployex.OpSysMock
+    |> expect(:run, fn _command, _options -> :ok end)
+
+    {:ok, index_live, _html} = live(conn, ~p"/applications")
+
+    assert index_live |> element("#app-restart-0") |> render_click() =~
+             "Are you sure you want to restart deployex?"
+
+    assert capture_log(fn ->
+             assert index_live |> element("#confirm-button-0", "Confirm") |> render_click()
+           end) =~ "Deployex was requested to terminate, see you soon!!!"
+
+    refute has_element?(index_live, "#cancel-button-0")
   end
 end
