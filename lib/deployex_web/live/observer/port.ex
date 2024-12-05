f46d5500badc71c89ce137993788d240622a22fa
Thiago Esteves <calori@gmail.com>
2024-12-05 11:09:34 -0300

    [BUG] Observer crashes if the process/port doesn't exist anymore (#90)

--- lib/deployex_web/live/observer/port.ex
+++ lib/deployex_web/live/observer/port.ex
@@ -4,13 +4,16 @@ defmodule DeployexWeb.Observer.Port do
   use DeployexWeb, :html
   use Phoenix.Component
 
+  alias DeployexWeb.Observer.Attention
+
   attr :info, :map, required: true
+  attr :id, :map, required: true
 
   def content(assigns) do
     info = assigns.info
 
     port_overview =
-      if info do
+      if is_map(info) do
         [
           %{name: "Id", value: "#{info.id}"},
           %{name: "Name", value: "#{info.name}"},
@@ -27,18 +30,24 @@ defmodule DeployexWeb.Observer.Port do
 
     ~H"""
     <div class="max-w-full rounded overflow-hidden shadow-lg">
-      <div :if={@info != nil} id="port_information">
-        <div class="flex grid grid-cols-3  gap-1 items-top">
-          <.table_process id="port-overview-table" title="Overview" rows={@port_overview}>
-            <:col :let={item}>
-              <span><%= item.name %></span>
-            </:col>
-            <:col :let={item}>
-              <%= item.value %>
-            </:col>
-          </.table_process>
-        </div>
-      </div>
+      <%= cond do %>
+        <% @info == nil -> %>
+        <% @info == :undefined -> %>
+          <Attention.content message={"Port #{@id} is either dead or protected and therefore can not be shown."} />
+        <% true -> %>
+          <div id="port_information">
+            <div class="flex grid grid-cols-3  gap-1 items-top">
+              <.table_process id="port-overview-table" title="Overview" rows={@port_overview}>
+                <:col :let={item}>
+                  <span><%= item.name %></span>
+                </:col>
+                <:col :let={item}>
+                  <%= item.value %>
+                </:col>
+              </.table_process>
+            </div>
+          </div>
+      <% end %>
     </div>
     """
   end
