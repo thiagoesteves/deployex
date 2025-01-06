329498567a312cd7090fbab74a75c0f4213ab475
Thiago Esteves <calori@gmail.com>
2025-01-06 14:47:54 -0300

    Updated main version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Deployex.MixProject do
   def project do
     [
       app: :deployex,
-      version: "0.3.0-rc24",
+      version: "0.3.0",
       elixir: "~> 1.15",
       name: "DeployEx",
       source_url: "https://github.com/thiagoesteves/deployex",
