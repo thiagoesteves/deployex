6968ba9b3d0192a2f45d398bc599c20821a6888d
Thiago Esteves <calori@gmail.com>
2025-06-12 14:19:19 -0300

    Improved login form

--- apps/deployex_web/lib/deployex_web/components/core_components.ex
+++ apps/deployex_web/lib/deployex_web/components/core_components.ex
@@ -422,7 +422,7 @@ defmodule DeployexWeb.CoreComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+          "px-3 mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
