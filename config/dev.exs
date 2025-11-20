3867e46d91fa8aec6a624ee9ac989bc7094b8da4
Thiago Esteves <thiago.esteves@erlang-solutions.com>
2025-11-20 17:10:31 -0300

    Adding short paths to dev target

--- config/dev.exs
+++ config/dev.exs
@@ -76,10 +76,8 @@ config :swoosh, :api_client, false
 
 config :foundation,
   env: "local",
-  install_path: nil,
-  log_path: nil,
-  var_path: "/tmp/deployex/var/lib/deployex",
-  monitored_app_log_path: "/tmp/deployex/var/log",
+  var_path: "/tmp/deployex/varlib",
+  monitored_app_log_path: "/tmp/deployex/varlog",
   monitoring: [
     memory: %{
       enable_restart: true,
