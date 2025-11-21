2755f3a558460995d37bdfde87a6954209ce56c0
Thiago Esteves <thiago.esteves@erlang-solutions.com>
2025-11-21 10:36:06 -0300

    Adding Initial proposed functions to hotupgrade

--- apps/deployer/lib/deployer/deployex.ex
+++ apps/deployer/lib/deployer/deployex.ex
@@ -3,6 +3,9 @@ defmodule Deployer.Deployex do
   This module contains deployex application specific commands
   """
 
+  alias Deployer.Upgrade
+  alias Deployer.Upgrade.Check
+  alias Deployer.Upgrade.Execute
   alias Host.Commander
 
   require Logger
@@ -21,11 +24,54 @@ defmodule Deployer.Deployex do
 
     :timer.sleep(sleep_time)
 
+    deployex_path = Application.fetch_env!(:foundation, :install_path)
+
     Commander.run(
-      "kill -9 $(ps -ax | grep \"/opt/deployex/erts-*.*/bin/beam.smp\" | grep -v grep | awk '{print $1}') ",
+      "kill -9 $(ps -ax | grep \"#{deployex_path}/erts-*.*/bin/beam.smp\" | grep -v grep | awk '{print $1}') ",
       [:sync, :stdout, :stderr]
     )
 
     :ok
+  end
+
+  @spec hot_upgrade(download_path :: String.t()) :: :ok
+  def hot_upgrade(download_path) do
+    deployex_path = Application.fetch_env!(:foundation, :install_path)
+    current_version = Application.spec(:deployer, :vsn)
+    node = Node.self()
+    name = "deployex"
+
+    [_, to_version] =
+      download_path
+      |> Path.basename(".tar.gz")
+      |> String.split("#{name}-")
+
+    Logger.warning("DeployEx hotupgrade from #{current_version} to #{to_version}")
+
+    # Temporary path to extract the release
+    new_path = "/tmp/hotupgrade/#{name}"
+    File.rm_rf(new_path)
+    File.mkdir_p(new_path)
+
+    check = %Check{
+      sname: name,
+      name: name,
+      language: "elixir",
+      download_path: download_path,
+      current_path: deployex_path,
+      new_path: new_path,
+      from_version: current_version,
+      to_version: to_version
+    }
+
+    with {"", 0} <- System.cmd("tar", ["-x", "-f", download_path, "-C", new_path]),
+         {:ok, :hot_upgrade} <- Upgrade.check(check),
+         %Execute{} = upgrade_data <- struct(%Execute{node: node}, Map.from_struct(check)),
+         :ok <- Upgrade.execute(upgrade_data) do
+      Logger.warning("Hot upgrade in deployex executed with success")
+    else
+      reason ->
+        Logger.error("Error while executing hotupgrade, reason: #{inspect(reason)}")
+    end
   end
 end
